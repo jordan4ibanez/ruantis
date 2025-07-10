3323a5024c481ed1a6c13cfe6e3189973f8fd922
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-10 13:22:02 -0400

    Deploy

--- source/init.ts
+++ source/init.ts
@@ -1,12 +1,14 @@
 import { whenPlayerJoins } from "./logic/player";
 import { deployTickTimer } from "./logic/tick";
 import { setUpCamera } from "./player/camera";
+import { deployDisplayHandling } from "./player/display";
 import { deployInventory } from "./player/inventory";
 import { deployTracker } from "./player/tracker";
 
 function main() {
 	deployTickTimer();
 	deployTracker();
+	deployDisplayHandling();
 
 	whenPlayerJoins((player: ObjectRef) => {
 		setUpCamera(player);
