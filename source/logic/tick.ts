d417202f3a171e7c0318effc02630dddb4ca3ded
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-10 11:28:59 -0400

    Add temporary counterpart

--- source/logic/tick.ts
+++ source/logic/tick.ts
@@ -14,7 +14,7 @@ const serverFunctions: serverTickFunctionType[] = [];
  * Run a function every client tick. (smoother)
  * @param func Function.
  */
-export function registerClientTickFunction(func: clientTickFunctionType) {
+export function registerClientTickFunction(func: clientTickFunctionType): void {
 	clientFunctions.push(func);
 }
 
@@ -22,8 +22,33 @@ export function registerClientTickFunction(func: clientTickFunctionType) {
  * Run a function every server tick. (lighter)
  * @param func Function.
  */
-export function registerServerTickFunction(func: serverTickFunctionType) {
+export function registerServerTickFunction(func: serverTickFunctionType): void {
 	serverFunctions.push(func);
+}
+
+type temporaryClientTickFunctionType = (
+	player: ObjectRef,
+	delta: number
+) => boolean;
+type temporaryServerTickFunctionType = (delta: number) => boolean;
+
+let functionID = 0;
+
+const temporaryClientFuncs = new Map<number, temporaryClientTickFunctionType>();
+const temporaryServerFuncs = new Map<number, temporaryServerTickFunctionType>();
+
+export function registerTemporaryClientTickFunction(
+	func: temporaryClientTickFunctionType
+): void {
+	temporaryClientFuncs.set(functionID, func);
+	functionID++;
+}
+
+export function registerTemporaryServerTickFunction(
+	func: temporaryServerTickFunctionType
+): void {
+	temporaryServerFuncs.set(functionID, func);
+	functionID++;
 }
 
 let serverTimer = 0;
