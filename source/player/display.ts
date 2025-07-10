7285b2fbdfa91feb1ae768b112d3c8f8469ec13d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-10 13:35:02 -0400

    Good to go

--- source/player/display.ts
+++ source/player/display.ts
@@ -1,26 +1,44 @@
 import { whenPlayerJoins } from "../logic/player";
-import { registerTargetedTemporaryServerTickFunction } from "../logic/tick";
+import {
+	registerServerTickFunction,
+	registerTargetedTemporaryClientTickFunction,
+} from "../logic/tick";
+import { getAllPlayers } from "./tracker";
 
 class DisplayInformation {}
 
 export function deployDisplayHandling(): void {
 	whenPlayerJoins((player) => {
-		registerTargetedTemporaryServerTickFunction(
+		// Get this data into memory as fast as possible.
+		registerTargetedTemporaryClientTickFunction(
 			player.get_player_name(),
 			(player) => {
 				const name = player.get_player_name();
 				const windowInfo = core.get_player_window_information(name);
-
 				if (windowInfo == null) {
-					print("not yet for", name);
 					return false;
 				}
 
-				print("got name");
+				print(dump(windowInfo));
+
+				print(windowInfo.size.x * 2);
 
 				return true;
 			}
 		);
+	});
+
+	// todo: convert this into a screen polling function.
+	registerServerTickFunction((delta) => {
+		for (const player of getAllPlayers()) {
+			const name = player.get_player_name();
+			const windowInfo = core.get_player_window_information(name);
+			if (windowInfo == null) {
+				continue;
+			}
+
+			print(dump(windowInfo.size));
+		}
 	});
 }
 
