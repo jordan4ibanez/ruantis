79ddaa1863003ebdaa93b6002be27ec500175d9a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-10 11:14:51 -0400

    This one needs a special function

--- source/player/display.ts
+++ source/player/display.ts
@@ -1,4 +1,18 @@
-class DisplayInformation {
+import { whenPlayerJoins } from "../logic/player";
+import { registerClientTickFunction } from "../logic/tick";
 
-}
+class DisplayInformation {}
 
+
+
+registerClientTickFunction((player: ObjectRef, delta: number) => {
+	const windowInfo = core.get_player_window_information(name);
+	if (windowInfo == null) {
+		print("going again");
+		return;
+	}
+
+	print("done");
+
+	return true;
+});
