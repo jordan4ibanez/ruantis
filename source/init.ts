365c2de3b61f13ab386e2b836c27d42ea6e75bf1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-11 11:55:41 -0400

    Update init.ts

--- source/init.ts
+++ source/init.ts
@@ -2,7 +2,6 @@ import { deployTickTimer } from "./logic/tick";
 import { deployCameraHandling } from "./player/camera";
 import { deployWindowHandling } from "./player/window";
 import { deployInventoryHandling } from "./player/inventory";
-
 import { deployTracker } from "./player/tracker";
 
 function main() {
