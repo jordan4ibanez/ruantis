5e73883fe06821e5db1c830078585247b14035be
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-11 05:26:51 -0400

    Just annihilate this file

--- source/player/inventory.ts
+++ source/player/inventory.ts
@@ -1,8 +1,4 @@
-import { whenPlayerJoins } from "../logic/player";
-import { registerServerTickFunction } from "../logic/tick";
-import { Vec2 } from "../utility/vector";
-import { getPlayerWindowInfo } from "./display";
-import { getAllPlayers } from "./tracker";
+import { registerWindowSizeChangeFunction } from "./window";
 
 core.register_on_player_receive_fields((player, formname) => {
 	// todo: force the inventory back open.
@@ -11,38 +7,13 @@ core.register_on_player_receive_fields((player, formname) => {
 // todo: maybe keep track of this? Could make movable windows in a horrific manor.
 
 export function deployInventoryHandling(): void {
-	whenPlayerJoins((player: ObjectRef) => {
-		deployInventory(player);
+	registerWindowSizeChangeFunction((player, windowInfo) => {
+		const name = player.get_player_name();
+
+		let hackjob = "formspec_version[9]";
+
+		print(windowInfo.size.toString());
+
+		player.set_inventory_formspec(hackjob);
 	});
-
-	registerServerTickFunction(() => {
-		for (const player of getAllPlayers()) {
-			createNewInventoryHackjob(player);
-		}
-	});
-}
-
-function deployInventory(player: ObjectRef): void {
-	assert(player.is_player());
-	createNewInventoryHackjob(player);
-}
-
-function createNewInventoryHackjob(player: ObjectRef): void {
-
-	print("hacking and jobbing")
-	player.set_inventory_formspec("fart");
-
-	const name = player.get_player_name();
-
-	const windowInfo = getPlayerWindowInfo(name);
-
-	// Cannot create an inventory if the window info does not exist.
-	if (windowInfo == null) {
-		return;
-	}
-
-	// No need to update.
-	if (!windowInfo.changed) {
-		return;
-	}
 }
