aa6c910f6e211fc92d0e5aac124285e33f729391
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-11 10:51:47 -0400

    Add a specialty function

--- source/logic/player.ts
+++ source/logic/player.ts
@@ -19,11 +19,34 @@ export function whenPlayerLeaves(func: pFunc): void {
 	leaveFuncs.push(func);
 }
 
+//? This is for when you need to do thing after the player is in the world.
+
+const afterJoinFuncs: pFunc[] = [];
+
+/**
+ * Add a function to run immediately after the player joins the server.
+ * @param func Function to run immediately after a player joins the server.
+ */
+export function afterPlayerJoins(func: pFunc): void {
+	afterJoinFuncs.push(func);
+}
+
 // And then the implementation.
 
 core.register_on_joinplayer((p: ObjectRef) => {
 	for (const f of joinFuncs) {
 		f(p);
+	}
+
+	for (const f of afterJoinFuncs) {
+		core.after(
+			0,
+			(f: pFunc, p: ObjectRef) => {
+				f(p);
+			},
+			f,
+			p
+		);
 	}
 });
 
