5cc444b0c23fede86a6fcf076875be957bcaec66
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-11 10:13:45 -0400

    smol

--- source/player/camera.ts
+++ source/player/camera.ts
@@ -1,5 +1,6 @@
 import { whenPlayerJoins } from "../logic/player";
 import { CameraModeType } from "../utility/enums";
+import { Vec3 } from "../utility/vector";
 
 function setUpCamera(player: ObjectRef): void {
 	assert(player.is_player());
@@ -11,6 +12,15 @@ function setUpCamera(player: ObjectRef): void {
 		minimap: true,
 	});
 	player.set_camera({ mode: CameraModeType.first });
+	const offset = player.get_properties().eye_height;
+	if (offset == null) {
+		throw new Error("Eye height was null");
+	}
+	player.set_eye_offset(
+		new Vec3(0, -offset * 10, 0),
+		new Vec3(0, -offset * 10, 0),
+		new Vec3(0, -offset * 10, 0)
+	);
 }
 
 export function deployCameraHandling(): void {
