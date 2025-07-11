c282a502d68fed00a2b771187d8b998ca48f87e7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-11 04:35:27 -0400

    This is just gonna be awful lol

--- source/player/inventory.ts
+++ source/player/inventory.ts
@@ -1,30 +1,48 @@
 import { whenPlayerJoins } from "../logic/player";
-import { registerClientTickFunction } from "../logic/tick";
+import { registerServerTickFunction } from "../logic/tick";
 import { Vec2 } from "../utility/vector";
-
-const displayResolutions = new Map<string, Vec2>();
+import { getPlayerWindowInfo } from "./display";
+import { getAllPlayers } from "./tracker";
 
 core.register_on_player_receive_fields((player, formname) => {
 	// todo: force the inventory back open.
 });
 
+// todo: maybe keep track of this? Could make movable windows in a horrific manor.
+
 export function deployInventoryHandling(): void {
 	whenPlayerJoins((player: ObjectRef) => {
 		deployInventory(player);
+	});
+
+	registerServerTickFunction(() => {
+		for (const player of getAllPlayers()) {
+			createNewInventoryHackjob(player);
+		}
 	});
 }
 
 function deployInventory(player: ObjectRef): void {
 	assert(player.is_player());
+	createNewInventoryHackjob(player);
+}
 
-	player.set_inventory_formspec("");
+function createNewInventoryHackjob(player: ObjectRef): void {
+
+	print("hacking and jobbing")
+	player.set_inventory_formspec("fart");
 
 	const name = player.get_player_name();
 
-	// print(name);
+	const windowInfo = getPlayerWindowInfo(name);
 
-	// core.register_globalstep(() => {
+	// Cannot create an inventory if the window info does not exist.
+	if (windowInfo == null) {
+		return;
+	}
 
-	// 	print(dump(windowInfo));
-	// });
+	// No need to update.
+	if (!windowInfo.changed) {
+		return;
+	}
 }
