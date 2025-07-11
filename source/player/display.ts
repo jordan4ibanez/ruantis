8d4c5aedd3d4d036572aba7423100d2543ca5e6a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-11 02:02:37 -0400

    Nice

--- source/player/display.ts
+++ source/player/display.ts
@@ -6,8 +6,11 @@ import {
 import { Vec2 } from "../utility/vector";
 import { getAllPlayers } from "./tracker";
 
-const winInfoGet = core.get_player_window_information;
+const prototype__winInfoGet = core.get_player_window_information;
 
+/**
+ * Holds window size in pixels and if the resolution changed.
+ */
 export class WindowInfo {
 	size: Vec2 = new Vec2();
 	changed: boolean;
@@ -17,6 +20,17 @@ export class WindowInfo {
 		this.size.y = init.y;
 		this.changed = true;
 	}
+}
+
+/**
+ * Gets the window information for a player.
+ */
+function getWinInfo(name: string): WindowInfo | null {
+	const data = prototype__winInfoGet(name);
+	if (data == null) {
+		return null;
+	}
+	return new WindowInfo(new Vec2(data.size.x, data.size.y));
 }
 
 const windowSizes = new Map<string, WindowInfo>();
@@ -29,13 +43,13 @@ export function deployDisplayHandling(): void {
 			(player) => {
 				const name = player.get_player_name();
 
-				const windowInfo = winInfoGet(name);
+				const windowInfo = getWinInfo(name);
 
 				if (windowInfo == null) {
 					return false;
 				}
 
-				windowSizes.set(name, new WindowInfo(windowInfo.size));
+				windowSizes.set(name, windowInfo);
 
 				return true;
 			}
@@ -46,7 +60,7 @@ export function deployDisplayHandling(): void {
 	registerServerTickFunction((delta) => {
 		for (const player of getAllPlayers()) {
 			const name = player.get_player_name();
-			const windowInfo = winInfoGet(name);
+			const windowInfo = prototype__winInfoGet(name);
 			// Player's window info has not been submitted by the client.
 			// If this starts hitting null randomly there is probably an issue with the client.
 			if (windowInfo == null) {
@@ -60,9 +74,7 @@ export function deployDisplayHandling(): void {
 				continue;
 			}
 
-			const changed = windowInfo.size.equals;
-
-			print(changed);
+			// print(changed);
 
 			// sizeInfo.x = windowInfo.size.x;
 			// sizeInfo.y = windowInfo.size.y;
