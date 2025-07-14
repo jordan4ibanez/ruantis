616bde63d68aa3b35adf32775d81540eba675fae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-14 14:25:29 -0400

    Load from barrel file

--- source/init.ts
+++ source/init.ts
@@ -4,7 +4,7 @@ import { deployWindowHandling } from "./player/window";
 import { deployInventoryHandling } from "./player/inventory";
 import { deployPlayerTracker } from "./player/tracker";
 import { deployPlayerEntity } from "./player/player";
-import { loadNodes } from "./tiles/debug_tiles";
+import { loadNodes } from "./tiles/tiles";
 
 function main() {
 	deployTickTimer();
