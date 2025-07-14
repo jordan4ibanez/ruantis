3e3b8e8058aa61ee0f6d05d9b7916f7cf1e35ca7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-14 14:43:31 -0400

    Some kind of hack job

--- source/tiles/tiles.ts
+++ source/tiles/tiles.ts
@@ -1,15 +1,43 @@
-import { PointedThingType } from "../utility/enums";
+import { ShallowVector3 } from "../../minetest-api";
+import { LogLevel, PointedThingType } from "../utility/enums";
 import { Vec3 } from "../utility/vector";
 
+const clickCheck = new Vec3(0, 1, 0);
+
+function tileClick(
+	position: ShallowVector3,
+	node: NodeTable,
+	puncher: ObjectRef | null,
+	pointedThing: PointedThing
+) {
+	if (puncher == null) {
+		return;
+	}
+	if (pointedThing.type != PointedThingType.node) {
+		throw new Error("Logic error.");
+	}
+
+	const above = new Vec3().copyFrom(pointedThing.above!);
+	const below = new Vec3().copyFrom(pointedThing.under!);
+
+	const diff = above.subtractImmutable(below);
+
+	const name = puncher.get_player_name();
+
+	if (!diff.equals(clickCheck)) {
+		core.log(LogLevel.warning, `Player ${name} is clicking under the map.`);
+		return;
+	}
+
+	const targetPos = new Vec3().copyFrom(position).add(clickCheck);
+
+	print(targetPos.toString());
+}
+
+// todo: make this into a ghost tile or something.
 core.register_node(":debug", {
 	tiles: ["ground.png"],
-	on_punch(position, node, puncher, pointedThing) {
-		if (pointedThing.type != PointedThingType.node) {
-			throw new Error("Logic error.");
-		}
-		const above = new Vec3().copyFrom(pointedThing.above!);
-		const below = new Vec3().copyFrom(pointedThing.under!);
-	},
+	on_punch: tileClick,
 });
 
 // export function registerTile(def: TileDefinition): void {
