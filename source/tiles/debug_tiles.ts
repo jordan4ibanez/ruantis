0de79a9b1bfc1c435be35d1845c98e9751907697
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-14 14:26:06 -0400

    Also debugging

--- source/tiles/debug_tiles.ts
+++ source/tiles/debug_tiles.ts
@@ -1,10 +1,6 @@
-core.register_node(":debug", {
-	tiles: ["ground.png"],
-});
-
 /**
  * Tree-shake removal function.
  *
  * Never use this!
  */
-export function loadNodes(): void {}
+export function loadDebugNodes(): void {}
