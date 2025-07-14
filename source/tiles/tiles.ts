ed3aa2b0b5019f659ca584803a3fb6776fb90dff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-14 14:31:43 -0400

    Let's see here

--- source/tiles/tiles.ts
+++ source/tiles/tiles.ts
@@ -1,7 +1,15 @@
-//? This is a barrel file.
+import { PointedThingType } from "../utility/enums";
+import { Vec3 } from "../utility/vector";
 
 core.register_node(":debug", {
 	tiles: ["ground.png"],
+	on_punch(position, node, puncher, pointedThing) {
+		if (pointedThing.type != PointedThingType.node) {
+			throw new Error("Logic error.");
+		}
+		const above = new Vec3().copyFrom(pointedThing.above!);
+		const below = new Vec3().copyFrom(pointedThing.under!);
+	},
 });
 
 // export function registerTile(def: TileDefinition): void {
