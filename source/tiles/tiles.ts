72040eca726ceb20c0354f46d67fcc6551633ea8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-14 14:29:21 -0400

    Start simplier

--- source/tiles/tiles.ts
+++ source/tiles/tiles.ts
@@ -1,25 +1,16 @@
 //? This is a barrel file.
 
-import { loadDebugNodes } from "./debug_tiles";
-
-interface TileDefinition {
-	name: string;
-	description?: string;
-}
-
 core.register_node(":debug", {
 	tiles: ["ground.png"],
 });
 
-export function registerTile(def: TileDefinition): void {
-	core.register_node(":" + def.name, def);
-}
+// export function registerTile(def: TileDefinition): void {
+// 	core.register_node(":" + def.name);
+// }
 
 /**
  * Tree-shake removal function.
  *
  * Never use this!
  */
-export function loadNodes(): void {
-	loadDebugNodes();
-}
+export function loadNodes(): void {}
