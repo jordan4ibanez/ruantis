75b8b8cdefcb0dfffe95415c6ad8cc6d0f619a4a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-27 08:23:57 -0400

    file reader & json parser

---
+++ source/utility/file.ts
@@ -0,0 +1,19 @@
+export function readFileToString(path: string): string {
+	const [f] = io.open(path, "rb");
+
+	if (f == undefined) {
+		throw new Error(`File ${path} does not exist.`);
+	}
+
+	let output: string = "";
+
+	for (const [line] of io.lines(path)) {
+		output += line + "\n";
+		// print(line);
+	}
+	return output;
+}
+
+export function parseJson(path: string) {
+	return core.parse_json(readFileToString(path));
+}
