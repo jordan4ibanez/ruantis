0674da8ccd9c33d0efaf352ffe235cb0e3b2354a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-27 06:11:49 -0400

    This module will be getting destroyed

--- source/player/camera.ts
+++ source/player/camera.ts
@@ -3,9 +3,9 @@ import { CameraModeType } from "../utility/enums";
 import { Vec3 } from "../utility/vector";
 import { Controls } from "./controls";
 
-
 function setUpCamera(client: ObjectRef): void {
 	assert(client.is_player());
+
 	client.hud_set_flags({
 		hotbar: false,
 		healthbar: false,
@@ -13,10 +13,11 @@ function setUpCamera(client: ObjectRef): void {
 		breathbar: false,
 		minimap: true,
 	});
+
 	client.set_physics_override({
 		speed: 1,
 		jump: 0,
-		gravity: 0,
+		gravity: 10000,
 		speed_climb: 0,
 		speed_crouch: 0,
 		liquid_fluidity: 0,
@@ -26,6 +27,7 @@ function setUpCamera(client: ObjectRef): void {
 		acceleration_air: 0,
 		sneak: false,
 	});
+
 	client.set_camera({ mode: CameraModeType.first });
 	const offset = client.get_properties().eye_height;
 	if (offset == null) {
