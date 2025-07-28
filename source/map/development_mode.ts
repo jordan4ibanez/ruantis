9c3a926fee38dce65e2bcad43bb71832b8e98b0f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 08:30:09 -0400

    Update development_mode.ts

--- source/map/development_mode.ts
+++ source/map/development_mode.ts
@@ -1,16 +1,25 @@
-
 //? Developer mode is a very specific feature to this game.
-//? 
+//?
 //? It allows automated regeneration of map chunks that become modified during development.
 //?
 //? If you're reading this and wondering how I designed this.
 //?
 //? I hand crafted everything you see and experience for you to enjoy.
 //?
+
+import { afterClientJoins } from "../logic/client_join_leave";
+import { HudElementType } from "../utility/enums";
+
 //? ~jordan4ibanez
 const devMode = true;
 
 if (devMode) {
+	afterClientJoins((client) => {
+		client.hud_add({
+			type: HudElementType.text,
+			text: "DEVELOPER MODE",
+		});
+	});
 }
 
 /**
