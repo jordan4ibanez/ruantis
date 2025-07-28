bd8371b9bada6de9b27a17555876c9fa7c2b4c69
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 04:21:35 -0400

    Update decorations.ts

--- source/map/decorations/decorations.ts
+++ source/map/decorations/decorations.ts
@@ -1,5 +1,12 @@
+import { Drawtype } from "../../utility/enums";
+import { registerBlock } from "../block_database";
 
-
+registerBlock({
+	name: "fountain",
+	drawtype: Drawtype.mesh,
+	mesh: "fountain.gltf",
+	tiles: ["fountain.png"],
+});
 
 /**
  * Tree-shake removal function.
