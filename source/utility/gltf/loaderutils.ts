31778b51a78cc2dd7b8776a93671b89cb88bb697
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-27 22:59:53 -0400

    format

--- source/utility/gltf/loaderutils.ts
+++ source/utility/gltf/loaderutils.ts
@@ -4,28 +4,30 @@
 declare var TextDecoder: any;
 
 export class LoaderUtils {
-    static decodeText(array: ArrayLike<number>): string {
-        /* istanbul ignore next */
-        if (typeof TextDecoder !== 'undefined') {
-            return new TextDecoder().decode(array);
-        }
+	static decodeText(array: ArrayLike<number>): string {
+		/* istanbul ignore next */
+		if (typeof TextDecoder !== "undefined") {
+			return new TextDecoder().decode(array);
+		}
 
-        // Avoid the String.fromCharCode.apply(null, array) shortcut, which
-        // throws a "maximum call stack size exceeded" error for large arrays.
-        let s = '';
-        for (const c of array as Array<number>) {
-            // Implicitly assumes little-endian.
-            s += String.fromCharCode(c);
-        }
+		// Avoid the String.fromCharCode.apply(null, array) shortcut, which
+		// throws a "maximum call stack size exceeded" error for large arrays.
+		let s = "";
+		for (const c of array as Array<number>) {
+			// Implicitly assumes little-endian.
+			s += String.fromCharCode(c);
+		}
 
-        // Merges multi-byte utf-8 characters.
-        return decodeURIComponent(escape(s));
-    }
+		// Merges multi-byte utf-8 characters.
+		return decodeURIComponent(escape(s));
+	}
 
-    static extractUrlBase(url: string) {
-        const parts = url.split('/');
-        if (parts.length === 1) { return './'; }
-        parts.pop();
-        return parts.join('/') + '/';
-    }
+	static extractUrlBase(url: string) {
+		const parts = url.split("/");
+		if (parts.length === 1) {
+			return "./";
+		}
+		parts.pop();
+		return parts.join("/") + "/";
+	}
 }
