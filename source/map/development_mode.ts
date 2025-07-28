714491bcb7a003552df783d8d94609ffdd820565
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 08:18:19 -0400

    A note to my future readers and players

--- source/map/development_mode.ts
+++ source/map/development_mode.ts
@@ -1,7 +1,13 @@
 
 //? Developer mode is a very specific feature to this game.
 //? 
-//? It allows automated generation of map
+//? It allows automated regeneration of map chunks that become modified during development.
+//?
+//? If you're reading this and wondering how I designed this.
+//?
+//? I hand crafted everything you see and experience for you to enjoy.
+//?
+//? ~jordan4ibanez
 const devMode = true;
 
 if (devMode) {
