2645b544098b289180d293de58d0976e2e7f8c17
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 06:24:45 -0400

    Update ore.ts

--- source/map/ore/ore.ts
+++ source/map/ore/ore.ts
@@ -1,4 +1,5 @@
 import { Drawtype, Nodeboxtype, TextureAlpha } from "../../utility/enums";
+import { Vec3 } from "../../utility/vector";
 import { registerBlock } from "../block_database";
 
 registerBlock({
@@ -12,6 +13,33 @@ registerBlock({
 		fixed: [-0.5, -0.5, -0.5, 0.5, 0.5, 0.5],
 	},
 });
+
+const ORE_MINE = "ore_mine";
+
+function on_dig(pos: Vec3) {
+	const meta = core.get_meta(pos);
+	let mine = meta.get_int(TREE_MINE);
+
+	if (mine == 0) {
+		mine = 10;
+		meta.set_int(TREE_MINE, mine);
+	}
+
+	mine -= 1;
+	meta.set_int(TREE_MINE, mine);
+
+	core.sound_play("tree_chop", { pos: pos });
+
+	// Could do an axe calculation here.
+	if (math.random(1, 1000) > 900) {
+		print("got logs");
+	}
+
+	if (mine == 1) {
+		setBlock(pos, "tree_stump");
+		core.get_node_timer(pos).start(serverTickRate * math.random(3, 10));
+	}
+}
 
 registerBlock({
 	name: "ore_coal",
