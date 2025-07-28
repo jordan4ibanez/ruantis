d50e97217c6ccf0be8a7fc13e257a49102bec3cd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 08:12:46 -0400

    Update development_mode.ts

--- source/map/development_mode.ts
+++ source/map/development_mode.ts
@@ -1,3 +1,12 @@
+
+//? Developer mode is a very specific feature to this game.
+//? 
+//? It allows automated generation of map
+const devMode = true;
+
+if (devMode) {
+}
+
 /**
  * Tree-shake removal function.
  *
