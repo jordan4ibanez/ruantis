9d3e45e8940e634fbecf4aa8134af488f344c26e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-27 21:51:58 -0400

    Fancy

--- source/utility/bitmanip.ts
+++ source/utility/bitmanip.ts
@@ -1,8 +1,3 @@
-export function numberToBinary(n: number): string {
-	let b = (n >>> 0).toString(2);
-	const bLen = b.length;
-	if (bLen < 8) {
-		b = b.padStart(8, "0");
-	}
-	return b;
+export function numberToBinary(n: number, bits: number = 8): string {
+	return (n >>> 0).toString(2).padStart(bits, "0");
 }
