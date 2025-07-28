e7e1fa601e63e1bbe789248936a61eaa8b17eef8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 04:05:27 -0400

    A stair blueprint

--- source/map/floor/stairs.ts
+++ source/map/floor/stairs.ts
@@ -1,3 +1,26 @@
+import { Drawtype, Nodeboxtype, ParamType2 } from "../../utility/enums";
+import { registerBlock, setBlock } from "../block_database";
+
+const stairBox: NodeBox = {
+	type: Nodeboxtype.fixed,
+	fixed: [
+		// Bottom step.
+		[0 - 0.5, 0 - 0.5, 0 - 0.5, 1 - 0.5, 0.25 - 0.5, 1 - 0.5],
+		[0 - 0.5, 0.25 - 0.5, 0.25 - 0.5, 1 - 0.5, 0.5 - 0.5, 1 - 0.5],
+		[0 - 0.5, 0.5 - 0.5, 0.5 - 0.5, 1 - 0.5, 0.75 - 0.5, 1 - 0.5],
+		// Top step.
+		[0 - 0.5, 0.75 - 0.5, 0.75 - 0.5, 1 - 0.5, 1 - 0.5, 1 - 0.5],
+	],
+};
+
+registerBlock({
+	name: "stone_stair",
+	paramtype2: ParamType2["4dir"],
+	drawtype: Drawtype.nodebox,
+	pointable: true,
+	node_box: stairBox,
+});
+
 /**
  * Tree-shake removal function.
  *
