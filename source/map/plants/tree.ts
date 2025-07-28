69efb51f43685e55db42bd40c673968c07103015
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 04:57:56 -0400

    A tree

--- source/map/plants/tree.ts
+++ source/map/plants/tree.ts
@@ -1,3 +1,17 @@
+import { Drawtype, Nodeboxtype } from "../../utility/enums";
+import { registerBlock } from "../block_database";
+
+registerBlock({
+	name: "tree",
+	drawtype: Drawtype.mesh,
+	mesh: "tree.gltf",
+	tiles: ["tree.png"],
+	collision_box: {
+		type: Nodeboxtype.fixed,
+		fixed: [-0.5, -0.5, -0.5, 0.5, 2.5, 0.5],
+	},
+});
+
 /**
  * Tree-shake removal function. (but there's still trees in here!)
  *
