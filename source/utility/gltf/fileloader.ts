76301222e8bdd936fb94d3469bc803702bbfd635
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-27 22:59:42 -0400

    unscrap

--- source/utility/gltf/fileloader.ts
+++ source/utility/gltf/fileloader.ts
@@ -1,108 +1,121 @@
 // Adapted from THEE.FileLoader
 // https://github.com/mrdoob/three.js/blob/master/src/loaders/FileLoader.js
 
-import { LoadingManager } from './loadingmanager';
+import { LoadingManager } from "./loadingmanager";
 
-// export type ProgressCallback = (xhr: XMLHttpRequest) => void;
-// export type XMLHttpRequestResponse = any;
+export type ProgressCallback = (xhr: XMLHttpRequest) => void;
+export type XMLHttpRequestResponse = any;
 
-// export class FileLoader {
-//     manager: LoadingManager;
-//     path: string | undefined;
-//     responseType: XMLHttpRequestResponseType;
-//     withCredentials: boolean;
-//     mimeType: string;
-//     requestHeaders: { [k: string]: string } ;
+export class FileLoader {
+	manager: LoadingManager;
+	path: string | undefined;
+	responseType: XMLHttpRequestResponseType;
+	withCredentials: boolean;
+	mimeType: string;
+	requestHeaders: { [k: string]: string };
 
-//     private runningRequests: { [url: string]: Promise<XMLHttpRequestResponse>} = {};
+	private runningRequests: {
+		[url: string]: Promise<XMLHttpRequestResponse>;
+	} = {};
 
-//     constructor(manager: LoadingManager) {
-//         this.manager = manager;
-//     }
-//     load(url: string, onProgress?: ProgressCallback): Promise<XMLHttpRequestResponse> {
-//         if (this.path !== undefined) { url = this.path + url; }
-//         url = this.manager.resolveURL(url);
+	constructor(manager: LoadingManager) {
+		this.manager = manager;
+	}
+	load(
+		url: string,
+		onProgress?: ProgressCallback
+	): Promise<XMLHttpRequestResponse> {
+		if (this.path !== undefined) {
+			url = this.path + url;
+		}
+		url = this.manager.resolveURL(url);
 
-//         if (this.runningRequests[url]) {
-//             return this.runningRequests[url];
-//         }
+		if (this.runningRequests[url]) {
+			return this.runningRequests[url];
+		}
 
-//         const promise = new Promise((resolve, reject) => {
-//             // TODO!!: Check for data: URI
-//             // (-> Safari can not handle Data URIs through XMLHttpRequest so process manually)
+		const promise = new Promise((resolve, reject) => {
+			// TODO!!: Check for data: URI
+			// (-> Safari can not handle Data URIs through XMLHttpRequest so process manually)
 
-//             // NOTE: Not using `fetch` because it doesn't support progress reporting
-//             const xhr = new XMLHttpRequest();
-//             xhr.open('GET', url, true);
-//             const self = this;
-//             xhr.onload = function(event: ProgressEvent) {
-//                 const response = this.response;
+			// NOTE: Not using `fetch` because it doesn't support progress reporting
+			const xhr = new XMLHttpRequest();
+			xhr.open("GET", url, true);
+			const self = this;
+			xhr.onload = function (event: ProgressEvent) {
+				const response = this.response;
 
-//                 /* istanbul ignore if */
-//                 if (this.status === 0) {
-//                     // Some browsers return HTTP Status 0 when using non-http protocol
-//                     // e.g. 'file://' or 'data://'. Handle as success.
-//                     console.warn('FileLoader: HTTP Status 0 received.');
-//                     resolve(response);
-//                     self.manager.itemEnd(url);
-//                 } else if (this.status === 200) {
-//                     resolve(response);
-//                     self.manager.itemEnd(url);
-//                 } else {
-//                     reject({
-//                         url,
-//                         status: this.status,
-//                         statusText: xhr.statusText,
-//                     });
+				/* istanbul ignore if */
+				if (this.status === 0) {
+					// Some browsers return HTTP Status 0 when using non-http protocol
+					// e.g. 'file://' or 'data://'. Handle as success.
+					console.warn("FileLoader: HTTP Status 0 received.");
+					resolve(response);
+					self.manager.itemEnd(url);
+				} else if (this.status === 200) {
+					resolve(response);
+					self.manager.itemEnd(url);
+				} else {
+					reject({
+						url,
+						status: this.status,
+						statusText: xhr.statusText,
+					});
 
-//                     self.manager.itemEnd(url);
-//                     self.manager.itemError(url);
-//                 }
+					self.manager.itemEnd(url);
+					self.manager.itemError(url);
+				}
 
-//                 delete self.runningRequests[url];
-//             };
+				delete self.runningRequests[url];
+			};
 
-//             xhr.onprogress = (xhr: any) => {
-//                 if (onProgress) {
-//                     onProgress(xhr);
-//                 }
-//             };
+			xhr.onprogress = (xhr: any) => {
+				if (onProgress) {
+					onProgress(xhr);
+				}
+			};
 
-//             /* istanbul ignore next */
-//             xhr.onerror = function(event: ErrorEvent) {
-//                 reject({
-//                     url,
-//                     status: this.status,
-//                     statusText: xhr.statusText,
-//                 });
-//                 self.manager.itemEnd(url);
-//                 self.manager.itemError(url);
+			/* istanbul ignore next */
+			xhr.onerror = function (event: ErrorEvent) {
+				reject({
+					url,
+					status: this.status,
+					statusText: xhr.statusText,
+				});
+				self.manager.itemEnd(url);
+				self.manager.itemError(url);
 
-//                 delete self.runningRequests[url];
-//             };
+				delete self.runningRequests[url];
+			};
 
-//             if (this.responseType) { xhr.responseType = this.responseType; }
-//             if (this.withCredentials) { xhr.withCredentials = this.withCredentials; }
-//             if (this.mimeType && xhr.overrideMimeType) {
-//                 xhr.overrideMimeType(this.mimeType !== undefined ? this.mimeType : 'text/plain');
-//             }
+			if (this.responseType) {
+				xhr.responseType = this.responseType;
+			}
+			if (this.withCredentials) {
+				xhr.withCredentials = this.withCredentials;
+			}
+			if (this.mimeType && xhr.overrideMimeType) {
+				xhr.overrideMimeType(
+					this.mimeType !== undefined ? this.mimeType : "text/plain"
+				);
+			}
 
-//             for (const header in this.requestHeaders) {
-//                 xhr.setRequestHeader(header, this.requestHeaders[header]);
-//             }
+			for (const header in this.requestHeaders) {
+				xhr.setRequestHeader(header, this.requestHeaders[header]);
+			}
 
-//             // tslint:disable-next-line:no-null-keyword
-//             xhr.send(null);
-//             this.manager.itemStart(url);
-//         });
+			// tslint:disable-next-line:no-null-keyword
+			xhr.send(null);
+			this.manager.itemStart(url);
+		});
 
-//         this.runningRequests[url] = promise;
+		this.runningRequests[url] = promise;
 
-//         return promise;
-//     }
+		return promise;
+	}
 
-//     setRequestHeader(key: string, value: string) {
-//         this.requestHeaders[key] = value;
-//         return this;
-//     }
-// }
+	setRequestHeader(key: string, value: string) {
+		this.requestHeaders[key] = value;
+		return this;
+	}
+}
