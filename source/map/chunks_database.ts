71687a898d6d2e7b64066daf8873f956f35b6ce0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 02:59:39 -0400

    Update chunks_database.ts

--- source/map/chunks_database.ts
+++ source/map/chunks_database.ts
@@ -2,20 +2,44 @@ import { afterClientJoins } from "../logic/client_join_leave";
 import { Drawtype } from "../utility/enums";
 import { Vec3 } from "../utility/vector";
 
-core.register_node(":chunk_template", {
-	drawtype: Drawtype.mesh,
-	mesh: "chunk_0_0.gltf",
-	tiles: ["chunk_0_0.png"],
-});
+interface PlacementData {
+	pos: Vec3;
+	block: string;
+}
 
-afterClientJoins(() => {
-	core.set_node(new Vec3(0, 0, 0), { name: "chunk_template" });
+interface Chunk {
+	pos: Vec3;
+	bocks: PlacementData[];
+}
 
-	core.set_node(new Vec3(1, 0, 0), { name: "debug" });
-	core.set_node(new Vec3(15, 0, 0), { name: "debug" });
-	core.set_node(new Vec3(15, 0, 15), { name: "debug" });
-	core.set_node(new Vec3(0, 0, 15), { name: "debug" });
-});
+const chunkDatabase = new Map<Vec3, Chunk>();
+
+function add(chunk: Chunk) {
+	chunkDatabase.set(chunk.pos, chunk);
+}
+
+function setUpData() {
+	const c_0_0: Chunk = {
+		pos: new Vec3(0, 0, 0),
+		bocks: [],
+	};
+
+	for (const x of $range(0, 15)) {
+		for (const z of $range(0, 15)) {
+			c_0_0.bocks.push({
+				pos: new Vec3(x, 0, z),
+				block: "",
+			});
+		}
+	}
+	add(c_0_0);
+}
+
+function deployWorld() {
+	setUpData();
+}
+
+core.register_on_mods_loaded(deployWorld);
 
 /**
  * Tree-shake removal function.
