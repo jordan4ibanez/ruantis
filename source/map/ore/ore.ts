7aed8a222b5f10cfa352247338fad3fd4a533928
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 06:12:13 -0400

    Blank ore

--- source/map/ore/ore.ts
+++ source/map/ore/ore.ts
@@ -1,3 +1,16 @@
+import { Drawtype, Nodeboxtype } from "../../utility/enums";
+import { registerBlock } from "../block_database";
+
+registerBlock({
+	name: "ore",
+	drawtype: Drawtype.mesh,
+	mesh: "ore.gltf",
+	tiles: ["ore_base.png", "blank.png"],
+	collision_box: {
+		type: Nodeboxtype.fixed,
+		fixed: [-0.5, -0.5, -0.5, 0.5, 0.5, 0.5],
+	},
+});
 
 /**
  * Tree-shake removal function.
