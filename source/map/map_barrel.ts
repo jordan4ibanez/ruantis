b858b7033b2cc5a11c9f68689b65e4b9e8136623
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-28 08:41:40 -0400

    Update map_barrel.ts

--- source/map/map_barrel.ts
+++ source/map/map_barrel.ts
@@ -1,4 +1,5 @@
 import { loadBlockDatabase } from "./block_database";
+import { ____automation_internal_only_deploy_map_data } from "./chunk_database/auto_chunk_data";
 import { loadDecorations } from "./decorations/decorations";
 import { loadDevelopmentMode } from "./development_mode";
 import { loadOutside } from "./floor/outside";
@@ -26,3 +27,5 @@ loadDecorations();
 loadTrees();
 
 loadOre();
+
+____automation_internal_only_deploy_map_data();
