120fa2c1a1374dd6d31e2618f13bca4092119e78
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 10:21:13 -0400

    Add a camp fire

--- source/map/decorations/decorations.ts
+++ source/map/decorations/decorations.ts
@@ -1,5 +1,8 @@
+import { ShallowVector3 } from "../../../minetest-api";
+import { serverTickRate } from "../../logic/tick";
 import { Drawtype, Nodeboxtype, ParamType2 } from "../../utility/enums";
-import { registerBlock } from "../block_database";
+import { registerBlock, setBlock } from "../block_database";
+import { devMode } from "../development_mode";
 
 registerBlock({
 	name: "fountain",
@@ -19,6 +22,40 @@ registerBlock({
 	mesh: "log_sideways.gltf",
 	tiles: ["log_sideways.png"],
 });
+
+for (const i of $range(1, 3)) {
+	registerBlock({
+		name: `camp_fire_${i}`,
+		drawtype: Drawtype.mesh,
+		mesh: `camp_fire_${i}.gltf`,
+		tiles: ["camp_fire.png"],
+	});
+
+	// Don't want this constantly changing in dev mode!
+	if (devMode) {
+		continue;
+	}
+
+	core.register_abm({
+		label: `:camp_fire_${i}`,
+		interval: serverTickRate,
+		nodenames: [`camp_fire_${i}`],
+		chance: 1,
+		action: function (
+			pos: ShallowVector3,
+			node: NodeTable,
+			delta: number
+		): void {
+			let t = i + 1;
+			if (t > 3) {
+				t = 1;
+			}
+			core.after(0, () => {
+				core.swap_node(pos, { name: `camp_fire_${t}` });
+			});
+		},
+	});
+}
 
 /**
  * Tree-shake removal function.
