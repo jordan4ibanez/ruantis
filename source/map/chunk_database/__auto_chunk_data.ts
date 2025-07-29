7582d444583059ce67f643ab4b75fcf1089c0657
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 06:15:58 -0400

    Time for attempt 2

--- source/map/chunk_database/__auto_chunk_data.ts
+++
@@ -1,4 +0,0 @@
-import { Vec3 } from "../../utility/vector";
-import { ____automation_internal_only_add_chunk } from "./chunks_database";
-
-export function ____automation_internal_only_automate_set_up_chunks() {}
