4669f47ba9025e8c0a01ddc82ee20ff8d3b5e494
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 00:30:06 -0400

    Set a fixed seed

--- source/map/static_generation.ts
+++ source/map/static_generation.ts
@@ -1,3 +1,9 @@
+import { ShallowVector3 } from "../../minetest-api";
+
+const FIXED_SEED = 18211451931165;
+
+core.register_on_generated((minp: ShallowVector3, maxp: ShallowVector3) => {});
+
 /**
  * Tree-shake removal function.
  *
