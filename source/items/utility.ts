302a9402527c8528326de0b7cb7f22a907fe4f78
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 14:30:08 -0400

    Default stack size to 1

--- source/items/utility.ts
+++ source/items/utility.ts
@@ -1,11 +1,13 @@
 export interface ItemDef {
 	name: string;
 	image: string;
+	stackLimit?: number;
 }
 export function registerItem(def: ItemDef) {
 	const temp: ItemDefinition = {
 		inventory_image: def.image,
 		wield_image: def.image,
+		stack_max: def.stackLimit || 1,
 	};
 
 	core.register_craftitem(":" + def.name, temp);
