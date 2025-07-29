1c3646558d2d3c6b3ea5c18177554906513fea05
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 01:35:54 -0400

    Update static_generation.ts

--- source/map/static_generation.ts
+++ source/map/static_generation.ts
@@ -8,87 +8,79 @@ const MULTIPLIER = 2;
 
 core.set_mapgen_setting("seed", FIXED_SEED, true);
 
-core.register_on_generated(
-	(minp: ShallowVector3, maxp: ShallowVector3, seed) => {
-		const noise: NoiseObject = core.get_value_noise({
-			seed: FIXED_SEED,
+core.register_on_generated((minp: ShallowVector3, maxp: ShallowVector3) => {
+	const noise: NoiseObject = core.get_value_noise({
+		seed: FIXED_SEED,
 
-			spread: new Vec3(FIXED_SPREAD, FIXED_SPREAD, FIXED_SPREAD),
-		});
+		spread: new Vec3(FIXED_SPREAD, FIXED_SPREAD, FIXED_SPREAD),
+	});
 
-		const worker = new Vec2();
+	const worker = new Vec2();
 
-		const outputQueue: Vec3[] = [];
+	const outputQueue: Vec3[] = [];
 
-		for (const x of $range(minp.x, maxp.x)) {
-			for (const z of $range(minp.z, maxp.z)) {
-				worker.x = x;
-				worker.y = z;
+	for (const x of $range(minp.x, maxp.x)) {
+		for (const z of $range(minp.z, maxp.z)) {
+			worker.x = x;
+			worker.y = z;
 
-				const rawNoise = noise.get_2d(worker);
+			const rawNoise = noise.get_2d(worker);
 
-				const fixedNoise = math.floor(rawNoise * MULTIPLIER);
+			const fixedNoise = math.floor(rawNoise * MULTIPLIER);
 
-				const output = new Vec3();
+			const output = new Vec3();
 
-				output.x = x;
-				output.y = fixedNoise;
-				output.z = z;
+			output.x = x;
+			output.y = fixedNoise;
+			output.z = z;
 
-				outputQueue.push(output);
-			}
-		}
-		core.bulk_set_node(outputQueue, { name: "i_grass" });
-
-		while (outputQueue.length > 0) {
-			outputQueue.pop();
-		}
-
-		// north +z
-		const front = new Vec3();
-		// south -z
-		const back = new Vec3();
-		// west -x
-		const left = new Vec3();
-		// east +x
-		const right = new Vec3();
-
-		for (const x of $range(minp.x, maxp.x)) {
-			for (const z of $range(minp.z, maxp.z)) {
-				worker.x = x;
-				worker.y = z;
-
-				front.x = x;
-				front.y = z + 1;
-
-				back.x = x;
-				back.y = z - 1;
-
-				left.x = x - 1;
-				left.y = z;
-
-				right.x = x + 1;
-				right.y = z;
-
-				const currentHeight = math.floor(
-					noise.get_2d(worker) * MULTIPLIER
-				);
-
-				const frontHeight = math.floor(
-					noise.get_2d(front) * MULTIPLIER
-				);
-
-				const backHeight = math.floor(noise.get_2d(back) * MULTIPLIER);
-
-				const leftHeight = math.floor(noise.get_2d(left) * MULTIPLIER);
-
-				const rightHeight = math.floor(
-					noise.get_2d(right) * MULTIPLIER
-				);
-			}
+			outputQueue.push(output);
 		}
 	}
-);
+	core.bulk_set_node(outputQueue, { name: "i_grass" });
+
+	while (outputQueue.length > 0) {
+		outputQueue.pop();
+	}
+
+	// north +z
+	const front = new Vec3();
+	// south -z
+	const back = new Vec3();
+	// west -x
+	const left = new Vec3();
+	// east +x
+	const right = new Vec3();
+
+	for (const x of $range(minp.x, maxp.x)) {
+		for (const z of $range(minp.z, maxp.z)) {
+			worker.x = x;
+			worker.y = z;
+
+			front.x = x;
+			front.y = z + 1;
+
+			back.x = x;
+			back.y = z - 1;
+
+			left.x = x - 1;
+			left.y = z;
+
+			right.x = x + 1;
+			right.y = z;
+
+			const currentHeight = math.floor(noise.get_2d(worker) * MULTIPLIER);
+
+			const frontHeight = math.floor(noise.get_2d(front) * MULTIPLIER);
+
+			const backHeight = math.floor(noise.get_2d(back) * MULTIPLIER);
+
+			const leftHeight = math.floor(noise.get_2d(left) * MULTIPLIER);
+
+			const rightHeight = math.floor(noise.get_2d(right) * MULTIPLIER);
+		}
+	}
+});
 
 /**
  * Tree-shake removal function.
