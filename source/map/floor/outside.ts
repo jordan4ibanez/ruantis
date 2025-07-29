9486390f8935788da05f1be32af4698785cd818f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 01:57:40 -0400

    Very basic sloped

--- source/map/floor/outside.ts
+++ source/map/floor/outside.ts
@@ -1,8 +1,11 @@
 import { afterClientJoins } from "../../logic/client_join_leave";
-import { Drawtype } from "../../utility/enums";
+import { Drawtype, ParamType2 } from "../../utility/enums";
 import { Vec3 } from "../../utility/vector";
 import { registerBlock, setBlock } from "../block_database";
 
+//? Regular grass blocks.
+
+// Randomizer.
 registerBlock({
 	name: "i_grass",
 	drawtype: Drawtype.airlike,
@@ -21,9 +24,31 @@ for (const i of $range(1, 3)) {
 	});
 }
 
-afterClientJoins((client) => {
-	setBlock(new Vec3(0, 0, 0), "i_grass");
+//? 1 angle grass block.
+
+// Randomizer.
+registerBlock({
+	name: "i_grass_single_slope",
+	drawtype: Drawtype.airlike,
+	groups: { static: 1 },
+	paramtype2: ParamType2["4dir"],
+	on_construct: (pos) => {
+		const i = math.random(1, 3);
+
+		setBlock(pos, `grass_single_slope_${i}`, core.get_node(pos).param2);
+	},
 });
+
+for (const i of $range(1, 3)) {
+	registerBlock({
+		name: `grass_single_slope_${i}`,
+		drawtype: Drawtype.mesh,
+		mesh: "grass_single_slope.gltf",
+		tiles: [`grass_${i}.png`],
+		groups: { static: 1 },
+		paramtype2: ParamType2["4dir"],
+	});
+}
 
 /**
  * Tree-shake removal function.
