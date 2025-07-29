6a11960e01eb3a210ca0f2c4681e2cf46a56e7bf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 13:02:02 -0400

    Update utility.ts

--- source/items/utility.ts
+++ source/items/utility.ts
@@ -5,7 +5,7 @@ export interface ItemDef {
 export function registerItem(def: ItemDef) {
 	const temp: ItemDefinition = {
 		inventory_image: def.image,
-		wield_image: "blank.png",
+		wield_image: def.image,
 	};
 
 	core.register_craftitem(":" + def.name, temp);
