96e4b48eaee1f5bba19874b63a81dabf6fc304f1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-29 00:39:55 -0400

    Very noisy

--- source/map/static_generation.ts
+++ source/map/static_generation.ts
@@ -1,8 +1,16 @@
 import { ShallowVector3 } from "../../minetest-api";
+import { Vec3 } from "../utility/vector";
 
 const FIXED_SEED = 18211451931165;
 
-core.register_on_generated((minp: ShallowVector3, maxp: ShallowVector3) => {});
+core.register_on_generated((minp: ShallowVector3, maxp: ShallowVector3) => {
+	const noise: NoiseObject = core.get_value_noise({
+		seed: FIXED_SEED,
+		spread: new Vec3(1, 1, 1),
+	});
+
+	print(dump(noise));
+});
 
 /**
  * Tree-shake removal function.
