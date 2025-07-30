d409e8c9ab32ff2cc30f29afa421213efa7ff059
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-30 01:13:04 -0400

    Move this code

--- source/player/inventory.ts
+++ source/player/inventory.ts
@@ -1,63 +1,36 @@
-import { afterClientJoins, whenClientJoins } from "../logic/client_join_leave";
-import { registerClientTickFunction } from "../logic/tick";
-import { Entity, registerEntity, spawnEntity } from "../utility/entity";
-import { EntityVisual } from "../utility/enums";
-import { Vec3 } from "../utility/vector";
+import { whenClientJoins } from "../logic/client_join_leave";
+import { devMode } from "../map/development_mode";
+import { HudElementType } from "../utility/enums";
+import { Vec2 } from "../utility/vector";
 
-// core.register_on_player_receive_fields((player, formname, fields) => {
-// 	print("hmm", "[", formname, "]", dump(fields));
-// });
+whenClientJoins((client) => {
+	const inv = client.get_inventory();
+	if (inv == null) {
+		throw new Error("Engine glitch with inventory.");
+	}
+	inv.set_size("main", 32);
 
-// todo: maybe keep track of this? Could make movable windows in a horrific manor.
+	// Wield slots.
+	inv.set_size("primary", 1);
+	inv.set_size("secondary", 1);
 
-registerClientTickFunction((player, delta) => {
-	// player.set_look_horizontal(0);
-	// player.set_look_vertical(0);
-	// print(player.get_player_control().movement_x);
+	if (devMode) {
+		client.hud_set_hotbar_itemcount(32);
+	} else {
+		client.hud_set_hotbar_itemcount(1);
+
+		// Not a hot bar. It just shows you what you have in your inventory.
+		client.hud_add({
+			type: HudElementType.inventory,
+			text: "main",
+			number: 32,
+			item: 0,
+			// offset: new Vec2(0.5, 1),
+			position: new Vec2(0.5, 1.0),
+			alignment: new Vec2(0.0, -1.0),
+		});
+	}
 });
-
-// registerWindowSizeChangeFunction((player, windowInfo) => {
-// const name = player.get_player_name();
-// //? I just kept testing things until something worked.
-// const magicNormalizedMultiplier = windowInfo.guiScale * 0.55;
-// const magicNumber = 96 * magicNormalizedMultiplier;
-// const width = windowInfo.size.x / magicNumber;
-// const height = windowInfo.size.y / magicNumber;
-// // Attempt to put a button in the middle of the screen.
-// const centerXPos = windowInfo.size.x / 2 / magicNumber;
-// const centerYPos = windowInfo.size.y / 2 / magicNumber;
-// //attempt to put a 1 pixel button on the screen
-// const pixel1 = math.floor((1 / magicNumber + 0.001) * 100) / 100;
-// let hackjob: string[] = [];
-// hackjob.push(
-// 	"formspec_version[9]" +
-// 		`size[${width},${height},true]` +
-// 		"position[0.5,0.5]" +
-// 		"anchor[0.5,0.5]" +
-// 		"no_prepend[]" +
-// 		"real_coordinates[true]" +
-// 		"padding[0,0]" +
-// 		"button[0,0;1,1;test;]" +
-// 		"box[0,0;1,1;test;]"
-// 	// "allow_close[false]" +
-// );
-// // print("started");
-// // for (const x of $range(-100, 100)) {
-// // 	for (const y of $range(-100, 100)) {
-// // 		hackjob.push(
-// // 			`button[${
-// // 				math.floor((centerXPos + x * pixel1) * 100) / 100
-// // 			},${
-// // 				math.floor((centerYPos + y * pixel1) * 100) / 100
-// // 			};${pixel1},${pixel1};${x},${y};]`
-// // 		);
-// // 	}
-// // }
-// // print("ended");
-// const output = hackjob.join("");
-// // print(output);
-// core.show_formspec(name, "current_player", output);
-// });
 
 /**
  * Tree-shake removal function.
--- source/player/player.ts
+++ source/player/player.ts
@@ -100,15 +100,6 @@ function setUpPlayer(client: ObjectRef): void {
 	client.set_stars({
 		visible: false,
 	});
-	const inv = client.get_inventory();
-	if (inv == null) {
-		throw new Error("Engine glitch with inventory.");
-	}
-	inv.set_size("main", 32);
-
-	// Wield slots.
-	inv.set_size("primary", 1);
-	inv.set_size("secondary", 1);
 
 	if (devMode) {
 		core.set_player_privs(client.get_player_name(), {
@@ -119,25 +110,9 @@ function setUpPlayer(client: ObjectRef): void {
 			grant: true,
 			server: true,
 		});
-
-		//? This can be changed to continue working on an area.
-		client.set_pos(new Vec3(0, 1, 0));
-		client.hud_set_hotbar_itemcount(32);
 	} else {
 		// This should be checking the meta to see if the player has been to the server before.
 		client.set_pos(new Vec3(0, 1, 0));
-		client.hud_set_hotbar_itemcount(1);
-
-		// Not a hot bar. It just shows you what you have in your inventory.
-		client.hud_add({
-			type: HudElementType.inventory,
-			text: "main",
-			number: 32,
-			item: 0,
-			// offset: new Vec2(0.5, 1),
-			position: new Vec2(0.5, 1.0),
-			alignment: new Vec2(0.0, -1.0),
-		});
 	}
 }
 
