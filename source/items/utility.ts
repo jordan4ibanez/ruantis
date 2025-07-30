5c8cbf9cf887e287d38274ba61f44240147e419d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-30 00:39:33 -0400

    Update utility.ts

--- source/items/utility.ts
+++ source/items/utility.ts
@@ -2,12 +2,14 @@ export interface ItemDef {
 	name: string;
 	image: string;
 	stackLimit?: number;
+	groups?: Dictionary<string, number>;
 }
 export function registerItem(def: ItemDef) {
 	const temp: ItemDefinition = {
 		inventory_image: def.image,
 		wield_image: def.image,
 		stack_max: def.stackLimit || 1,
+		groups: def.groups,
 	};
 
 	core.register_craftitem(":" + def.name, temp);
