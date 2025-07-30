74cab3920d1ddc2aa25030fbefd3913bbbf996ca
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-30 08:02:46 -0400

    Implement levels

---
+++ source/player/levels.ts
@@ -0,0 +1,19 @@
+// Level 0 is what you start out at.
+const levels: number[] = [0];
+
+(() => {
+	let baseLevel = 0;
+	let adder = 100;
+	// Goes to level 100;
+	for (let i = 0; i < 100; i++) {
+		baseLevel += adder;
+		levels.push(baseLevel);
+		adder = math.floor(adder * 1.1);
+	}
+})();
+
+for (const i of $range(0, levels.length - 1)) {
+	print(`Level: [${i}] = ${levels[i]}`);
+}
+
+export function getLevel(): void {}
