ef6064bef6bab2d52394ad0c62a1090f5a7ef9d8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-08-01 17:48:35 -0400

    And clean this mess up

--- source/init.ts
+++ source/init.ts
@@ -1,19 +1,11 @@
 import { deployTickTimer } from "./logic/tick";
-import { deployWindowHandling } from "./player/window";
-import { deployInventoryHandling } from "./player/inventory";
-import { deployClientTracker } from "./player/tracker";
-import { deployPlayerEntity } from "./player/player";
 import { loadNodes } from "./map/map_barrel";
 import { loadItems } from "./items/item_barrel";
-import { deployCommandOverrides } from "./player/command_overrides";
+import { deployPlayer } from "./player/player_barrel";
 
 function main() {
 	deployTickTimer();
-	deployClientTracker();
-	deployWindowHandling();
-	deployInventoryHandling();
-	deployCommandOverrides();
-	deployPlayerEntity();
+	deployPlayer();
 	loadNodes();
 	loadItems();
 }
